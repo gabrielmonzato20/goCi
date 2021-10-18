e6ecb75144d097e063545e1a0ee174bf60bca876
gabriel <gabrielmonzato20@gmail.com>
2021-10-18 05:53:36 -0300

    fix: change the  message of error

--- math_test.go
+++ math_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestSoma(t *testing.T){
   total := soma(10,100)
   if total != 110{
-t.Errorf("Resultado da soma invalid %d , Expect %d",total , 110)
+t.Errorf("Result of sum is  invalid %d , Expect %d",total , 110)
   }
 
 }
