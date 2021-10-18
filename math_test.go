00b5450c9c4a3135f9227cbb0304dbd7d382f4d1
gabriel <gabrielmonzato20@gmail.com>
2021-10-18 05:36:10 -0300

    fix: :zap: fix bug

--- math_test.go
+++ math_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestSoma(t *testing.T){
-  total := Soma(10,100)
+  total := soma(10,100)
   if total != 110{
 t.Errorf("Resultado da soma invalid %d , Expect %d",total , 110)
   }
