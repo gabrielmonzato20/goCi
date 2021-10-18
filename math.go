840907d072041bfd99e2fcfef953c56224567a63
gabriel <gabrielmonzato20@gmail.com>
2021-10-18 05:34:12 -0300

    fix: :bug: change pack to run

--- math.go
+++ math.go
@@ -2,9 +2,9 @@ package main
 import "fmt"
 
 func main(){
-fmt.Println(Soma(100, 100))
+fmt.Println(soma(100, 100))
 }
 
-func Soma(a int , b int ) int {
+func soma(a int , b int ) int {
 return a+b 
 }
